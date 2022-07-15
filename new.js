b3ff15cef500e17bae6a8d1f1db15f3f7fe7e8bf
Swayam <swayammaheshwari890@gmail.com>
2022-07-15 12:47:44 +0530

    first c opertaion in mongoose

--- new.js
+++ new.js
@@ -4,26 +4,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash")
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
+
+let authorName = "req.body.authorNameR";
+let postTitle = "req.body.postTitleR";
+let postBody = "req.body.postBodyR";
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/newblogDB", {useNewUrlParser: true});
+
+const blogSchema = {
+  author: String,
+  title: String,
+  poster: String,
+  date: Date
+}
+
+const Post = mongoose.model("Post", blogSchema);
 
 app.get("/",(req,res)=>{
-    res.render("index",{author: authorName, title : postTitle, post:postBody});
+  Post.find({}, function(err, posts){
+    res.render("index", {
+      posts: posts
+      });
+  });
 })
 
 app.post("/compose",(req,res)=>{
-  const authorName = req.body.authorNameR;
-  const postTitle = req.body.postTitleR;
-  const postBody = req.body.postBodyR;
-
+  const post = new Post({
+    author:req.body.authorNameR,
+    title: req.body.postTitleR,
+    poster: req.body.postBodyR
+  });
+   post.save();
   res.redirect("/")
 })
+
 app.get("/author",(req,res)=>{
     res.render("author");
 })
